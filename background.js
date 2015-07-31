06a3eb3f3c3e656afbb5080d2b6fe51553492aef
Christopher Brandt <xtopher.brandt@gmail.com>
2015-07-31 10:49:21 -0700

    Reload the Pivotal Tracker tab, if open.

--- background.js
+++ background.js
@@ -3,3 +3,22 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
 
   sendResponse( {outputActivityChecked: localStorage["outputActivityChecked"], format: localStorage["format"] } );
 });
+
+// Check whether new version is installed
+chrome.runtime.onInstalled.addListener(function(details){
+    if(details.reason == "install"){
+        console.log("This is a first install!");
+    }else if(details.reason == "update"){
+        var thisVersion = chrome.runtime.getManifest().version;
+        console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+    }
+
+    // regardless of the reason, reload the Pivotal Tracker tab
+    chrome.tabs.query({
+      "url":"https://www.pivotaltracker.com/*"
+    }, function( tabs ){
+      for ( var i = 0; i < tabs.length; i++ ){
+        chrome.tabs.reload( tabs[i].id );
+      }
+    });
+});
